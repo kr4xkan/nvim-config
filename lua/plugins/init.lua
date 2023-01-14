81c8d3bf50da720ddc0df846e868c54fe9015622
Hugo Baptista <kr4xkan@Hydra.lan>
2023-01-14 09:03:16 +0100

    i tried

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,15 @@
-vim.cmd([[packadd packer.nvim]])
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
@@ -43,6 +54,10 @@ return require("packer").startup(function(use)
 	})
 	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
 	use("dstein64/vim-startuptime")
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 
 	require("plugins/startup")
 end)
